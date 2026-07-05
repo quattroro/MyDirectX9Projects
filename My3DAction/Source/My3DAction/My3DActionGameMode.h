740205006509ca7626d099387c621f888484123b
jo <kio7147@naver.com>
2026-07-05 20:22:53 +0900

    1234

---
+++ My3DAction/Source/My3DAction/My3DActionGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "My3DActionGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMy3DActionGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMy3DActionGameMode();
+};
+
+
+
